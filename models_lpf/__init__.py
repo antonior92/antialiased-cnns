88946cf2cca6cfffe0864c19d3d94faa44f095d2
Antonio Horta Ribeiro <antonior92@gmail.com>
2020-07-15 18:36:06 -0300

    add Downsample to models_lpf/__init__.py

--- models_lpf/__init__.py
+++ models_lpf/__init__.py
@@ -9,8 +9,9 @@ from .densenet import *
 from .mobilenet import *
 from .resnet import *
 from .vgg import *
+from .downsample import Downsample
 
 __all__ = ['AlexNet', 'alexnet', 'DenseNet', 'densenet121', 'densenet169', 'densenet201', 'densenet161',
            'MobileNetV2', 'mobilenet_v2', 'ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152', 'resnext50_32x4d', 'resnext101_32x8d', 'VGG', 'vgg11', 'vgg11_bn', 'vgg13',
-           'vgg13_bn', 'vgg16', 'vgg16_bn', 'vgg19_bn', 'vgg19']
+           'vgg13_bn', 'vgg16', 'vgg16_bn', 'vgg19_bn', 'vgg19', 'Downsample']
